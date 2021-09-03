73a0273c86783a49e6f76cb442de11bcfe21396c
kelvins-io <mybaishati@gmail.com>
2021-09-03 22:38:45 +0800

    优化app启动流程

--- build-run.sh
+++ build-run.sh
@@ -8,7 +8,6 @@ echo 开始构建
 go build -o micro-mall-order-cron main.go
 
 cp -n ./etc/app.ini.example ./etc/app.ini
-mkdir -p logs
 
 echo 开始运行micro-mall-order-cron
 nohup ./micro-mall-order-cron -s start >nohup.out  2>&1  &
